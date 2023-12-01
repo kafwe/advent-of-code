c372aa28e2afa10d67d798e26abedff587943d9d
Jordy Kafwe <jordykafwe@gmail.com>
2023-12-01 15:03:20 +0200

    Add part 1

--- day01/day1.go
+++ day01/day1.go
@@ -1,8 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"unicode"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Part 1:", part1())
+}
 
+func part1() int {
+	file, err := os.Open("./day01/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		total += getNumber(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return total
+}
+
+func getNumber(line string) int {
+	digits := ""
+	for _, char := range line {
+		if unicode.IsDigit(char) {
+			digits += string(char)
+		}
+	}
+
+	number, err := strconv.Atoi(string(digits[0]) + string(digits[len(digits)-1]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return number
 }
