5827162e4b9b6d0d90789007c3c23e12964df626
Jordy Kafwe <jordykafwe@gmail.com>
2023-12-01 18:58:31 +0200

    Add part 2

--- day01/day1.go
+++ day01/day1.go
@@ -6,11 +6,25 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
+var spelledDigits = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
 func main() {
 	fmt.Println("Part 1:", part1())
+	fmt.Println("Part 2:", part2())
 }
 
 func part1() int {
@@ -33,11 +47,40 @@ func part1() int {
 	return total
 }
 
+func part2() int {
+	file, err := os.Open("./day01/input2.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		processedLine := processSpelledNumbers(scanner.Text())
+		total += getNumber(processedLine)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return total
+}
+
+func processSpelledNumbers(line string) string {
+	for spelledDigit, digit := range spelledDigits {
+		line = strings.ReplaceAll(line, spelledDigit,
+			spelledDigit[:1]+digit+spelledDigit[len(spelledDigit)-1:])
+	}
+	return line
+}
+
 func getNumber(line string) int {
-	digits := ""
+	digits := []rune{}
 	for _, char := range line {
 		if unicode.IsDigit(char) {
-			digits += string(char)
+			digits = append(digits, char)
 		}
 	}
 
