168c8f7aad7a4c77a24a7949100f2b8a711dbda5
Jordy Kafwe <jordykafwe@gmail.com>
2023-12-07 22:58:18 +0200

    Add Day 7 Part 2

--- day07/day7.go
+++ day07/day7.go
@@ -4,14 +4,17 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"slices"
+	"strings"
 )
 
 type handStrength int
 
 const (
-	HighCard handStrength = iota + 1
+	Unknown handStrength = iota
+	HighCard
 	OnePair
 	TwoPair
 	ThreeOfAKind
@@ -21,28 +24,64 @@ const (
 )
 
 type hand struct {
-	cards string
-	bid   int
-	kind  handStrength
+	cards      string
+	bid        int
+	kind       handStrength
+	jokerCards string
+}
+
+var cardStrength = map[byte]int{
+	'A': 13, 'K': 12, 'Q': 11, 'T': 9, '9': 8, '8': 7,
+	'7': 6, '6': 5, '5': 4, '4': 3, '3': 2, '2': 1, 'J': 0,
 }
 
 func main() {
-	hands := readHands("./day07/input.txt")
-	slices.SortStableFunc(hands, compareHands)
+	if len(os.Args) < 2 {
+		log.Fatal("Missing input file argument")
+	}
+	file := os.Args[1]
+	hands := readHands(file)
 
+	slices.SortStableFunc(hands, compareHands)
+	fmt.Println("Part 1:", calculateWinnings(hands))
+	slices.SortStableFunc(hands, compareJokerHands)
+	fmt.Println("Part 2:", calculateWinnings(hands))
+}
+
+func calculateWinnings(hands []hand) int {
 	winnings := 0
 	for rank, h := range hands {
 		winnings += h.bid * (rank + 1)
 	}
-	fmt.Println("Part 1:", winnings)
+	return winnings
 }
 
 func compareHands(hand1, hand2 hand) int {
-	cardStrength := map[byte]int{
-		'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8,
-		'8': 7, '7': 6, '6': 5, '5': 4, '4': 3, '3': 2, '2': 1,
+	hand1.kind, hand2.kind = determineHandKind(hand1.cards), determineHandKind(hand2.cards)
+
+	switch {
+	case hand1.kind == hand2.kind:
+		for i := range hand1.cards {
+			if cardStrength[hand1.cards[i]] > cardStrength[hand2.cards[i]] {
+				return 1
+			} else if cardStrength[hand1.cards[i]] < cardStrength[hand2.cards[i]] {
+				return -1
+			}
+		}
+	case hand1.kind > hand2.kind:
+		return 1
+	case hand1.kind < hand2.kind:
+		return -1
 	}
-	hand1.kind, hand2.kind = determineHandKind(hand1), determineHandKind(hand2)
+	return 0
+}
+
+func compareJokerHands(hand1, hand2 hand) int {
+	// turn 'J' card into Joker
+	cardStrength['J'] = 0
+	hand1.processJoker()
+	hand2.processJoker()
+	hand1.kind, hand2.kind = determineHandKind(hand1.jokerCards), determineHandKind(hand2.jokerCards)
 
 	switch {
 	case hand1.kind == hand2.kind:
@@ -87,38 +126,54 @@ func readHands(filePath string) []hand {
 	return hands
 }
 
-func determineHandKind(h hand) handStrength {
+func determineHandKind(hand string) handStrength {
 	labels := make(map[rune]int)
-	for _, c := range h.cards {
+	for _, c := range hand {
 		labels[c]++
 	}
-	var kind handStrength
 
 	switch len(labels) {
 	case 5:
-		kind = HighCard
+		return HighCard
 	case 4:
-		kind = OnePair
+		return OnePair
 	case 3:
 		for _, n := range labels {
-			if n == 3 {
-				kind = ThreeOfAKind
-			}
-			if n == 2 {
-				kind = TwoPair
+			switch n {
+			case 3:
+				return ThreeOfAKind
+			case 2:
+				return TwoPair
 			}
 		}
 	case 2:
 		for _, n := range labels {
-			if n == 1 {
-				kind = FourOfAKind
-			}
-			if n == 2 {
-				kind = FullHouse
+			switch n {
+			case 1:
+				return FourOfAKind
+			case 2:
+				return FullHouse
 			}
 		}
 	case 1:
-		kind = FiveOfAKind
+		return FiveOfAKind
 	}
-	return kind
+	return Unknown
+}
+
+func (h *hand) processJoker() {
+	labels := make(map[rune]int)
+	for _, c := range h.cards {
+		labels[c]++
+	}
+
+	maxCount := math.MinInt64
+	var label rune
+	for l, n := range labels {
+		if n > maxCount && l != 'J' {
+			maxCount = n
+			label = l
+		}
+	}
+	h.jokerCards = strings.ReplaceAll(h.cards, "J", string(label))
 }
