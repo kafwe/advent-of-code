afe32b142513bf0d4cec9dd8456d0f44ba0030ae
kafwe <jordykafwe@gmail.com>
2024-12-01 03:25:58 +0200

    Add file reading util

---
+++ fileio/reader.go
@@ -0,0 +1,26 @@
+package fileio
+
+import (
+	"bufio"
+	"os"
+)
+
+// ReadLines reads a file and returns its lines as a slice of strings.
+ func ReadLines(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+ }
