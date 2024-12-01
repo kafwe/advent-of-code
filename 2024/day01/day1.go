d5501eb1fbe8d84649f7e6b21423974df088ada9
kafwe <jordykafwe@gmail.com>
2024-12-01 15:07:29 +0200

    Add 2024 day 1 part 2 solution

--- 2024/day01/day1.go
+++ 2024/day01/day1.go
@@ -27,30 +27,58 @@ func main() {
 			fmt.Println("Error converting parts[0]:", err)
 			continue
 		}
-
 		val2, err := strconv.Atoi(parts[1])
 		if err != nil {
 			fmt.Println("Error converting parts[1]:", err)
 			continue
 		}
 
-		l[i] = val1
-		r[i] = val2
+		l[i], r[i] = val1, val2
 	}
 
+	fmt.Println("Part 1:", part1(l, r))
+	fmt.Println("Part 2:", part2(l, r))
+
+}
+
+func part1(l []int, r []int) int {
 	sort.Ints(l)
 	sort.Ints(r)
 
 	var dist float64
-
 	for i, _ := range l {
 		dist += math.Abs(float64(l[i]) - float64(r[i]))
 	}
-
-	fmt.Println(int(dist))
-
+	return int(dist)
 }
 
-func part1() int {
+func part2(l []int, r []int) int {
+	left := make(map[int]int)
+	right := make(map[int]int)
+
+	for _, num := range l {
+		_, checked := left[num]
+		left[num]++
+		// already counted num occurences in right list
+		if checked {
+			continue
+		}
+
+		for _, val := range r {
+			if val == num {
+				right[num]++
+			}
+		}
+
+	}
+
+	score := 0
+	for k, v := range left {
+		for i := 0; i < v; i++ {
+			score += right[k] * k
+		}
+	}
+
+	return score
 
 }
